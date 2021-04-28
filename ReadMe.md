83bbde5ac31c081d4eb2286ce46cec629e77d6ea
uchida-at-599580918920 <zZeYqaufusGfdnc1+tZoTNpt7sJWTjjx6tHfHMUPHbI=>
2021-04-28 22:48:22 +0900

    new commit

---
+++ ReadMe.md
@@ -0,0 +1,15 @@
+# Yahoo!ジオコーダAPIを使い住所リストに座標を付与するスクリプト
+
+- 環境
+  
+ `PHP 7.2.2 で動作確認`
+  
+
+
+- 使い方 
+
+ CSVファイルを指定しaddressTocoodinate.phpを実行する
+
+
+  `php addressTocoodinate.php < hogehoge.csv > result.csv`
+  
---
+++ addressTocodinate.php
@@ -0,0 +1,28 @@
+<?php
+$google_maps_api_key = "";
+$google_endpoint = "https://maps.googleapis.com/maps/api/geocode/json?";
+
+// 標準入力を一行ずつ配列に代入します
+while ($line = fgets(STDIN)) {
+    $address = str_replace(array("\r\n", "\r", "\n"), '', $line);
+    $params = http_build_query(array(
+        "language" => "ja",
+        "address" => $address,
+        "key" => $google_maps_api_key
+    ));
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $google_endpoint.$params);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $google_result =  curl_exec($curl);
+
+    if ($google_result) {
+        $json = json_decode($google_result);
+        if ($json->status == "OK") {
+            echo $address . "," . $json->results[0]->geometry->location->lng . "," . $json->results[0]->geometry->location->lat . "\n";
+        }
+    }
+}
+
+
+?>
